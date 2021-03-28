709ce9e7487c3650148607b46531c765fb1bbdd6
Sultan-B <57250403+Sultan-Bibarys@users.noreply.github.com>
2021-03-28 22:26:30 +0600

    Delete backend/routes/routes directory

--- backend/routes/routes/index.js
+++
@@ -1,9 +0,0 @@
-const Router = require("express");
-const router = new Router()
-const userRouter = require('./userRouter')
-const productRouter = require('./productRouter')
-
-router.use('/user', userRouter)
-router.use('/product', productRouter)
-
-module.exports = router
--- backend/routes/routes/userRouter.js
+++
@@ -1,9 +0,0 @@
-const Router = require("express");
-const router = new Router()
-const userController = require('../controllers/userContoller')
-
-router.post('/registration', userController.registration)
-router.post('/login', userController.auth)
-router.get('/auth', userController.check)
-
-module.exports = router;
